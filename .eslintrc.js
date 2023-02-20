2e548da21d4925a5206448754b8cbc7a144aae8e
Okabe-TO <ttt.sao.619@gmail.com>
2023-02-21 03:09:21 +0900

    add eslint, prettier, node-sass and fix program

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+		'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
